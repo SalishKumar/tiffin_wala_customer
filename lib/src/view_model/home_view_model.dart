22bcb58beec40dd8a8717d15cda236ee6cfd8304
Nasir Khalil <nasirkhalil447@gmail.com>
2022-01-29 20:20:24 +0500

    check

--- lib/src/view_model/home_view_model.dart
+++ lib/src/view_model/home_view_model.dart
@@ -4,13 +4,10 @@ import 'package:tiffin_wala_customer/src/models/user.dart';
 
 class HomeViewModel extends ChangeNotifier {
   TextEditingController searchController = TextEditingController();
-  final storage = new FlutterSecureStorage();
+  
   late User1 user;
   
-  logout() async{
-    await storage.deleteAll();
-  }
-
+  
   search(){
     // print(val + '*');
     // searchController.text=val;
